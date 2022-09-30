03fdaf795b762056a91161472e013e23c165af38
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-09-30 18:20:03 +0530

    feat: added home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 
 function App() {
   return (
  <>
    <Navbar/>
-
+   <Home />
  </>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+   <div>
+   <div class="card bg-dark text-white border-0">
+     <img src="/assests/hero.jpg" class="card-img" alt="Background"  height="550px"/>
+        <div class="card-img-overlay d-flex flex-column justify-content-center">
+            <div class="container">
+            <h5 class="card-title display-3 fw-bold">Smell Exotic</h5>
+            <p class="card-text">Get perfumes according to all your moods and occasions.</p>
+            </div>
+        </div>
+  </div>
+  </div>
+  )
+}
+
+export default Home
